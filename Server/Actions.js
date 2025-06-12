567ff9274d7bcf15e7830d5b5308b2aa8c2e2a65
aloknegi786 <aloksinghnegi005@gmail.com>
2025-06-13 01:30:59 +0530

    added more actions related to role setup

--- Server/Actions.js
+++ Server/Actions.js
@@ -1,6 +1,8 @@
 export const ACTIONS = {
     JOIN: 'join',
     JOINED: 'joined',
+    PROMOTE: 'promote',
+    REQUEST_PROMOTION: 'request',
     DISCONNECTED: 'disconnected',
     CODE_CHANGE: 'code-change',
     SYNC_CODE: 'sync-code',
