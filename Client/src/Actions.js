b98a46096b5dfcb94ea72653ae942b12f11897c5
aloknegi786 <aloksinghnegi005@gmail.com>
2025-06-14 00:37:49 +0530

    introduced promote event

--- Client/src/Actions.js
+++ Client/src/Actions.js
@@ -1,7 +1,9 @@
 const ACTIONS = {
     JOIN: 'join',
     JOINED: 'joined',
-    DISCONNECTED: 'disconnected',
+    PROMOTE: 'promote',
+    REQUEST_PROMOTION: 'request',
+    DISCONNECTED: 'disconnect',
     CODE_CHANGE: 'code-change',
     SYNC_CODE: 'sync-code',
     LEAVE: 'leave',
